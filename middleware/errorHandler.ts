396346b35e7d586d3f92f29c1b0f3c5879bee917
Buyan-Urt.kh <kgbuyna@gmail.com>
2024-12-19 17:28:02 +0800

    middleware type error fix

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -1,10 +1,22 @@
-import { AppContext } from "../types/base.ts";
+import { Middleware } from "@oak/oak/middleware";
+import { AppContext, ContextState, id } from "../types/base.ts";
 
-export const errorHandler = async (
-  ctx: AppContext,
+export const errorHandler: Middleware<
+  ContextState,
+  AppContext<ContextState, Record<string, any>>
+> // AppState
+ = async (
+  ctx: AppContext<ContextState, Record<string, any>>,
   next,
 ) => {
   try {
+    ctx.response.body = {
+      status: "success",
+      message: "Hello, World!",
+    };
+    ctx.state.userId = 1 as id;
+    // ctx.response.status = ""
+    // console.log(ctx.state.);
     await next(); // Pass to the next middleware
   } catch (err) {
     console.error("Error occurred:", err);
